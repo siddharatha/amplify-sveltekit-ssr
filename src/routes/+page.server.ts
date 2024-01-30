4cfb2851a1622244ff0893b50030cfe483abef5a
Sid <siddharatha.nagavarapu@se.com>
2024-01-30 17:58:44 +0100

    console output

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,14 @@
 import type { PageServerLoad } from './$types';
 
-export const load = (({ setHeaders }) => {
+export const load = ((e) => {
 	const randomNumber = Math.random();
 
 	// cache-control 2 minutes
 
-	setHeaders({
-		'cache-control': 'public, max-age=120'
-	});
-
+	// setHeaders({
+	// 	'cache-control': 'public, max-age=120'
+	// });
+	console.log(e.url);
 	return {
 		randomNumber
 	};
--- src/routes/api/endpoint/+server.ts
+++ src/routes/api/endpoint/+server.ts
@@ -1,12 +1,14 @@
 import { json, type RequestHandler } from '@sveltejs/kit';
 
-export const GET = (({ setHeaders }) => {
+export const GET = ((e) => {
 	const randomNumber = Math.random();
 
 	// cache-control 2 minutes
-	setHeaders({
-		'cache-control': 'public, max-age=120'
-	});
+	// setHeaders({
+	// 	'cache-control': 'public, max-age=120'
+	// });
+	console.log(e)
+	console.log(e.platform);
 
 	return json({ randomNumber });
 }) satisfies RequestHandler;
